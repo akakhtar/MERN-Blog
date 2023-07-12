7565f46c3134e5a7ac6ce20cfe51999e6d383aa7
Atif <akakhtarsbg@gmail.com>
2023-07-12 22:18:31 +0530

    Intial commit

---
+++ api/index.js
@@ -0,0 +1,139 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+const cookieParser = require("cookie-parser");
+const { default: mongoose } = require("mongoose");
+const User = require("./models/User");
+const Post = require("./models/Post");
+const app = express();
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const uploadMiddleware = multer({ dest: "uploads/" });
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+const fs = require("fs");
+
+const salt = bcrypt.genSaltSync(10);
+const secret = process.env.SECRET;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.CONNECTION_LINK);
+app.use("/uploads", express.static(__dirname + "/uploads"));
+app.post("/register", async function (req, res) {
+  const { username, password } = req.body;
+  try {
+    const userDoc = await User.create({
+      username,
+      password: bcrypt.hashSync(password, salt),
+    });
+    console.log("testing......");
+    res.json(userDoc);
+  } catch (e) {
+    res.status(400).json(e);
+  }
+});
+
+app.post("/login", async function (req, res) {
+  const { username, password } = req.body;
+  const userDoc = await User.findOne({ username });
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+  if (passOk) {
+    jwt.sign({ username, id: userDoc.id }, secret, {}, (err, token) => {
+      if (err) throw err;
+      res.cookie("token", token).json({
+        id: userDoc._id,
+        username,
+      });
+    });
+  } else {
+    res.status(400).json("wrong credentials");
+  }
+});
+
+app.get("/profile", (req, res) => {
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, (err, info) => {
+    if (err) throw err;
+    res.json(info);
+  });
+});
+
+app.post("/logout", (req, res) => {
+  res.cookie("token", "").json("ok");
+});
+
+app.post("/post", uploadMiddleware.single("file"), async (req, res) => {
+  const { originalname, path } = req.file;
+  const parts = originalname.split(".");
+  const ext = parts[parts.length - 1];
+  const newPath = path + "." + ext;
+  fs.renameSync(path, newPath);
+
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, async (err, info) => {
+    if (err) throw err;
+    const { title, summary, content } = req.body;
+    const postDoc = await Post.create({
+      title,
+      summary,
+      content,
+      cover: newPath,
+      author: info.id,
+    });
+    res.json(postDoc);
+  });
+});
+
+app.put("/post", uploadMiddleware.single("file"), async (req, res) => {
+  let newPath = null;
+  if (req.file) {
+    const { originalname, path } = req.file;
+    const parts = originalname.split(".");
+    const ext = parts[parts.length - 1];
+    newPath = path + "." + ext;
+    fs.renameSync(path, newPath);
+
+    const { token } = req.cookies;
+    jwt.verify(token, secret, {}, async (err, info) => {
+      if (err) throw err;
+      const { id, title, summary, content } = req.body;
+      const postDoc = await Post.findById(id);
+      const isAuthor =
+        JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+      if (!isAuthor) {
+        return res.status(400).json("you are not the author");
+      }
+      await postDoc.updateOne({
+        title,
+        summary,
+        content,
+        cover: newPath ? newPath : postDoc.cover,
+      });
+      res.json(postDoc.cover);
+    });
+  }
+});
+
+app.get("/post", async (req, res) => {
+  res.json(
+    await Post.find()
+      .populate("author", ["username"])
+      .sort({ createdAt: -1 })
+      .limit(20)
+  );
+});
+
+app.get("/post/:id", async (req, res) => {
+  const { id } = req.params;
+  const postDoc = await Post.findById(id).populate("author", ["username"]);
+  res.json(postDoc);
+});
+
+app.listen(4000, function () {
+  console.log("Server running on port 4000");
+});
+//mongodb+srv://akakhtarsbg:atif4409@cluster0.szmlwck.mongodb.net/?retryWrites=true&w=majority
